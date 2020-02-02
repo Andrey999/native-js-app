0c790e569070fdea2f8b8823f11bbd480a4eec04
Andrey999 <gromovandrey1985@gmail.com>
2020-02-02 14:04:56 +0300

    add index.html, app.js, utils.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import  { isValid } from './utils.js';
 import './style.css';
 
 const form = document.getElementById('form');
@@ -5,7 +6,21 @@ const input = document.getElementById('input__question');
 const button = document.getElementById('submit__btn');
 
 form.addEventListener('submit', submitFormHandler);
+input.addEventListener('input', () => {
+  button.disabled = !isValid(input.value);
+});
 
 function submitFormHandler(event) {
-  event.preventDaefault();
+  event.preventDefault();
+  
+  if(isValid(input.value)) {
+    const question = {
+      text: input.value.trim(),
+      date: new Date().toJSON()
+    }
+    console.log('question: ', question);
+    input.value = '';
+    input.className = '';
+    // async request to server question
+  }
 }
