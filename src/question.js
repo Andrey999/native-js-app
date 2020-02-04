1b569edfc6da0d6b6e50f183ef1681b2a549d2b0
Andrey999 <gromovandrey1985@gmail.com>
2020-02-04 13:41:21 +0300

    deploy project
    Add firebase hosting

--- src/question.js
+++ src/question.js
@@ -1,5 +1,5 @@
 export class Question {
-
+///////////////////////////
   static create(question) {
     return fetch('https://question-me-ec248.firebaseio.com/questions.json', {
       method: 'POST',
@@ -13,17 +13,60 @@ export class Question {
         return question;
       })
       .then(addToLocaleStorage)
+      .then(Question.renderList)
+  }
+//////////////////////////
+  static renderList() {
+    const questions = getItemFromStorage();
+    const html = questions.length > 0 ? 
+    questions.map(toCard).join('')
+    : `<div class="mui--text-headline">Вопросов нет</div>`;
+
+    const list = document.getElementById('list');
+    list.innerHTML = html;
+  }
+///////////////////////////
+  static fetchData(token) { 
+    if(!token){
+      return Promise.resolve('<p class="error>В доступе отказано</p>')
+    }
+   return fetch(`https://question-me-ec248.firebaseio.com/questions.json?auth=${token}`)
+    .then(response => response.json())
+    .then(response => {
+      if(response && response.error) {
+        return `<p class="error>${response.error}</p>`
+      }
+ 
+      return response ? Object.keys(response).map(item => ({
+        ...response[item],
+        id: item
+      })) : []
+    })
+  }
+///////////////////////////
+  static allQuestionToHtml(question) {
+    return question.length > 0 ? question.map(question => {
+      return `
+      <div>${question.text}</div>
+      `;
+    }) 
+    : `<p>Вопросов нет</p>`;
   }
 }
-
+////////////////////////////////////////////////////////////////////////
 function addToLocaleStorage(question) {
-  let store = getItemFromStorage();
-  console.log(store);
-  
+  const store = getItemFromStorage();
   store.push(question);
   localStorage.setItem('question', JSON.stringify(store));
 }
-
+//////////////////////////////////////////////////////////////////////////
 function getItemFromStorage() {
   return JSON.parse(localStorage.getItem('question') || '[]');
+}
+//////////////////////////////////////////////////////////////////////
+function toCard(question) {
+  return `
+        <div class="mui--text-black-54">${new Date(question.date).toLocaleDateString()} ${new Date(question.date).toLocaleTimeString()}</div>
+        <div>${question.text}</div>
+        <br>`;
 }
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,14 @@
 export function isValid(value) {
   return value.length >= 10;
 }
+/////////////////////////////////////////////
+export function createModal(title, content) {
+  const modal = document.createElement('div');
+  modal.classList.add('modal');
+  const html = `
+  <h1>${title}</h1>
+  <div class="modal__content">${content}</div>`;
+  modal.innerHTML = html;
+
+  mui.overlay('on', modal);
+}
